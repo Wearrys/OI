6bfb0ed67d5dfe3e35272d91b94373195677ad7d
wearrys <1721604476@qq.com>
2018-03-08 09:59:37 +0800

    new contest

--- Contests/CodePlus/2018-3/C.cpp
+++ Contests/CodePlus/2018-3/C.cpp
@@ -9,7 +9,7 @@ typedef pair<int, int> pii;
 
 #define fst first
 #define snd second
-#define pb(a) push_back(a)
+#define pb push_back
 #define mp(a, b) std::make_pair(a, b)
 #define debug(...) fprintf(stderr, __VA_ARGS__)
 
@@ -31,22 +31,47 @@ template <typename T> T read(T& x) {
     return x *= f;
 }
 
-const int N = 100;
+const int B = 500;
+const int N = 500000;
 const int mo = 1e9 + 7;
 
-int n, m;
-int binom[N + 5][N + 5];
-int dp[N + 5][N + 5][N + 5];
+int fac[N + 5], inv[N + 5];
+int binom(int n, int k) {
+    if(n < k || n < 0 || k < 0) return 0;
+    return 1ll * fac[n] * inv[k] % mo * inv[n - k] % mo;
+}
 
 int fpm(int x, int y) {
     int res = 1;
     for(; y > 0; y >>= 1) {
         if(y & 1)
             res = 1ll * res * x % mo;
-        x = 1ll * x * x %mo;
+        x = 1ll * x * x % mo;
     }
     return res;
 }
+
+int f[N / B + 5];
+int query(int n, int m) {
+    if(!n) return 0;
+
+    int ans = f[(n - 1) / B];
+    for(int i = (n - 1) / B * B + 1; i < n; ++i) {
+        ans = (ans + binom(m, i)) % mo;
+    }
+    return ans;
+}
+
+void init() {
+    fac[0] = 1;
+    for(int i = 1; i <= N; ++i) fac[i] = 1ll * fac[i-1] * i % mo;
+    inv[N] = fpm(fac[N], mo - 2);
+    for(int i = N; i >= 1; --i) inv[i-1] = 1ll * inv[i] * i % mo;
+}
+
+int ans[N + 5];
+struct node { int d, n, m, id; };
+vector<node> que[N + 5];
 
 int main() {
 #ifdef Wearry
@@ -54,38 +79,31 @@ int main() {
     freopen("out", "w", stdout);
 #endif
 
-    int t, p;
-    read(t), read(p);
+    int T, n, m, p;
+    init();
+    read(T), read(p);
 
-    for (int i = 0; i <= N; ++i) {
-        binom[i][0] = binom[i][i] = 1;
-        for (int j = 1; j < i; ++j) binom[i][j] = (binom[i-1][j] + binom[i-1][j-1]) % mo;
-    }
-
-    while(t--) {
+    for(int i = 0; i < T; ++i) { 
         read(n), read(m);
-        memset(dp, 0, sizeof dp);
-
-        int ans = 0;
-        dp[0][0][0] = 1;
-        for (int i = 0; i <= n + m; ++i) {
-            for (int j = 0; j <= n; ++j) {
-                for (int k = 0; k <= i; ++k) {
-                    if(j < n) 
-                        (dp[i+1][j+1][k+1] += dp[i][j][k]) %= mo;
-                    (dp[i+1][j][std::max(k - 1, 0)] += dp[i][j][k]) %= mo;
-                }
-            }
-
-            if(i == n + m) {
-                for(int j = 0; j <= i; ++j) {
-                    ans = (ans + 1ll * dp[i][n][j] * j % mo) % mo;
-                }
-            }
+        if(n >= m) {
+            que[n + m].pb((node){ n-m, m, n, i });
+        } else {
+            que[n + m].pb((node){ 0, n, m, i });
         }
-        ans = 1ll * ans * fpm(binom[n + m][n], mo - 2) % mo;
-        printf("%d\n", ans);
     }
+
+    int pw = 1;
+    for(int i = 0; i <= N; ++i) {
+        if(i % B == 0) { f[i / B] = pw; }
+        for(int j = 0; j*B < i; ++j) 
+            f[j] = (2ll*f[j] - binom(i-1, j*B) + mo) % mo;
+        pw = (pw << 1) % mo;
+
+        for(auto q : que[i]) {
+            ans[q.id] = (q.d + 1ll*fpm(binom(i, q.n), mo-2) * query(q.n, i)) % mo;
+        }
+    }
+    for(int i = 0; i < T; ++i) printf("%d\n", ans[i]);
 
     return 0;
 }
---
+++ Docs/knowledge/bernoulli_number.md
@@ -0,0 +1,56 @@
+% Bernoulli Number and Power Sum
+% Wearry
+
+## Problem Description
+
+&emsp;&emsp;Calculate: 
+
+$$ S_k(n) = \sum_{i = 0}^{n - 1} i^k $$
+
+## Transform
+
+$$ 
+\begin{aligned}
+S_{m+1}(n) + n^{m+1} &= S_{m+1}(n + 1) \\
+                     &= \sum_{k = 0}^{n - 1} (k + 1)^{m+1} \\
+                     &= \sum_{k = 0}^{m + 1} {m + 1 \choose k} S_k(n)
+\end{aligned}
+\eqno(1.1)
+$$
+
+$$ 
+\mathrm{Let} \,\, \hat{S}_k(n) = \frac{1}{k + 1} \sum_{j = 0}^{k} {k + 1 \choose j} B_j n ^ {k + 1 - j} \eqno(1.2)
+$$
+
+$$
+\mathrm{Where} \,\, {B_i} \,\, \mathrm{follow:} \sum_{j = 0}^{m} {m + 1 \choose j} B_j = [m = 0] \eqno(1.3)
+$$
+
+## Proof
+
+$$ \hat{S}_k(n) = S_k(n) $$ 
+
+&emsp;&emsp;
+Considering proof below by induction, assume that $\forall \,\, 0 \le j < m: \hat(S)_j(n) = S_j(n)$, 
+now to proof $\hat{S}_m(n) = S_k(n)$.
+
+&emsp;&emsp; 
+Minus $S_{m+1}(n)$ from both left and right part of equation(1.1):
+
+$$ 
+\begin{aligned}
+n^{m+1} &= \sum_{k = 0}^{m} {m + 1 \choose k} S_k(n) \\
+        &= \sum_{k = 0}^{m} {m + 1 \choose k} \hat{S}_k(n) + (m+1)\Delta \\
+        &= \sum_{0 \le j \le k \le m} {m + 1 \choose k} {k + 1 \choose k - j} \frac{B_{k-j} n^{j+1}}{k + 1} + (m+1)\Delta \\
+        &= \sum_{k = 0}^{m} {m + 1 \choose k} \frac{1}{k+1} \sum_{j = 0}^{k} {k + 1 \choose j + 1} B_{k-j}n^{j+1} + (m+1)\Delta \\
+        &= \sum_{k = 0}^{m} {m + 1 \choose k} \sum_{j = 0}^{k} {k \choose j} \frac{B_{k-j}n^{j+1}}{j+1} + (m+1)\Delta \\
+        &= \sum_{j = 0}^{m} {m + 1 \choose j} \frac{1}{j+1} n^{j+1} \sum_{k = j}^{m} B_{k-j} {m + 1 - j \choose k-j} + (m+1)\Delta \\
+        &= n^{m + 1} + (m+1)\Delta
+\end{aligned}
+$$
+
+&emsp;&emsp;&emsp;&emsp;
+&emsp;&emsp;&emsp;&emsp;
+&emsp;&emsp;&emsp;&emsp;
+&emsp;&emsp;&emsp;&emsp;
+$\Delta = 0, \mathrm{Q.E.D.}$
