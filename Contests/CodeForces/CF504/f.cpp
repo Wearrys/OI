9c7f5b4eb2f48de686583a7eddb95936d6bf136a
wearrys <1721604476@qq.com>
2018-08-20 09:37:03 +0800

    cf504f

--- Contests/CodeForces/CF504/f.cpp
+++ Contests/CodeForces/CF504/f.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-typedef long long LL;
+using std::pair;
+using std::vector;
+using std::string;
+
+typedef long long ll;
 typedef pair<int, int> pii;
 
 #define fst first
 #define snd second
-#define pb push_back
+#define pb(a) push_back(a)
+#define mp(a, b) std::make_pair(a, b)
+#define debug(...) fprintf(stderr, __VA_ARGS__)
 
 template <typename T> bool chkmax(T& a, T b) { return a < b ? a = b, 1 : 0; }
 template <typename T> bool chkmin(T& a, T b) { return a > b ? a = b, 1 : 0; }
 
-const int N = (int) 1e6;
 const int oo = 0x3f3f3f3f;
 
 template<typename T> T read(T& x) {
@@ -22,94 +26,57 @@ template<typename T> T read(T& x) {
     return x *= f;
 }
 
+const int N = (int) 5e5;
+const int M = (int) 1e6;
+
 int n, k, m;
 struct edge {
     int u, v, w;
-
     bool operator < (const edge& rhs) const {
         return w < rhs.w;
     }
 };
 
 edge e[N + 5];
+int fw[N + 5];
+int fa[N + 5][21], dep[N + 5];
+int st[N + 5], to[M + 5], nxt[M + 5], ecnt = 1;
 
-namespace LCT {
-
-#define TY(u) (c[fa[u]][1] == u) 
-#define rt(u) (c[fa[u]][0] != u && c[fa[u]][1] != u)
-    
-    bool rev[N + 5];
-    int val[N + 5], tag[N + 5];
-    int fa[N + 5], c[N + 5][2];
-
-    void clear() {
-        memset(val, oo, sizeof val);
-        memset(tag, oo, sizeof tag);
-    }
-
-    inline void push_down(int u) {
-        if(rev[u]) {
-            rev[u] = 0;
-            swap(c[u][0], c[u][1]);
-
-            rev[c[u][0]] ^= 1;
-            rev[c[u][1]] ^= 1;
-        }
-
-        if(c[u][0]) {
-            chkmin(tag[c[u][0]], tag[u]);
-            chkmin(val[c[u][0]], tag[u]);
-        }
-        if(c[u][1]) {
-            chkmin(tag[c[u][1]], tag[u]);
-            chkmin(val[c[u][1]], tag[u]);
-        }
-        tag[u] = oo;
-    }
-
-    inline void rotate(int u) {
-        int v = fa[u], w = fa[v];
-        bool t = (c[v][1] == u);
-
-        if(!rt(v)) c[w][c[w][1] == v] = u;
-        c[v][t] = c[u][t^1]; fa[c[u][t^1]] = v;
-        c[u][t^1] = v; fa[v] = u; fa[u] = w;
-    }
-
-    inline void Push(int u) {
-        if(!rt(u)) Push(fa[u]);
-        push_down(u);
-    }
-
-    inline void splay(int u) {
-        Push(u);
-        while(!rt(u)) {
-            if(!rt(fa[u]))
-                rotate(TY(u) == TY(fa[u]) ? fa[u] : u); 
-            rotate(u);
-        }
-    }
-
-    inline int access(int u) {
-        int v;
-        for(v = 0; u; u = fa[v = u]) {
-            splay(u);
-            c[u][1] = v;
-        }
-        return v;
-    }
-    
-    inline void set_rt(int u) { rev[access(u)] ^= 1; splay(u); }
-    inline void link(int u, int v) { set_rt(u); fa[u] = v; }
-    inline int query(int u) { splay(u); return val[u]; }
-    inline void modify(int u, int v, int w) { set_rt(u); access(v); splay(v); chkmin(tag[v], w); }
+inline void addedge(int u, int v) {
+    to[++ ecnt] = v; nxt[ecnt] = st[u]; st[u] = ecnt; 
+    to[++ ecnt] = u; nxt[ecnt] = st[v]; st[v] = ecnt;
 }
 
-int fa[N + 5];
+void dfs(int u, int f = 0) {
+    fa[u][0] = f;
+    dep[u] = dep[f] + 1;
+    for(int i = 1; i < 21; ++i) fa[u][i] = fa[fa[u][i-1]][i-1];
+    for(int i = st[u]; i; i = nxt[i]) {
+        int v = to[i];
+        if(v == f) continue;
+        dfs(v, u);
+        fw[v] = i >> 1;
+    }
+}
+
+int lca(int x, int y) {
+    if(dep[x] < dep[y]) {
+        std::swap(x, y);
+    }
+    for(int i = 20; i >= 0; --i) 
+        if(dep[fa[x][i]] >= dep[y]) { x = fa[x][i]; }
+    if(x == y) return x;
+    for(int i = 20; i >= 0; --i) 
+        if(fa[x][i] != fa[y][i]) { x = fa[x][i], y = fa[y][i]; }
+    return fa[x][0];
+}
+
+int pa[N + 5];
 int findset(int x) {
-    return fa[x] == x ? x : fa[x] = findset(fa[x]);
+    return pa[x] == x ? x : pa[x] = findset(pa[x]);
 }
 
+int ans[N + 5];
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("in", "r", stdin);
@@ -117,42 +84,46 @@ int main() {
 #endif
 
     read(n), read(k), read(m);
-    for(int i = 1; i <= n; ++i) fa[i] = i;
-
-    int tot = n;
+    for(int i = 1; i <= n; ++i) pa[i] = i;
     for(int i = 1; i <= k; ++i) {
-        static int x, y;
-        read(x), read(y);
-        if(findset(x) != findset(y)) {
-            ++ tot;
-            fa[findset(x)] = findset(y);
-            LCT::link(x, tot); LCT::link(y, tot);
-        }
+        static int x, y; read(x), read(y);
+        addedge(x, y), pa[findset(x)] = findset(y);
     }
-    for(int i = 1; i <= m; ++i) { read(e[i].u), read(e[i].v), read(e[i].w); }
+    for(int i = 1; i <= m; ++i) read(e[i].u), read(e[i].v), read(e[i].w); 
 
-    LCT::clear();
-    sort(e + 1, e + m + 1);
+    std::sort(e + 1, e + m + 1);
 
     for(int i = 1; i <= m; i++) {
         int u = e[i].u, v = e[i].v;
-
         if(findset(u) ^ findset(v)) {
-            ++ tot;
-            LCT::link(u, tot);
-            LCT::link(tot, v);
-            fa[findset(u)] = findset(v);
-        } else {
-            LCT::modify(u, v, e[i].w); 
+            e[i].w = -1;
+            addedge(u, v);
+            pa[findset(u)] = findset(v);
         }
     }
 
-    long long ans = 0;
-    for(int i = 1; i <= k; ++i) {
-        int tmp = LCT::query(i + n);
-        if(tmp == oo) return puts("-1"), 0;
-        ans += tmp;
+    for(int i = 1; i <= n; ++i) pa[i] = i;
+    for(int i = 1; i <= n; ++i) if(!dep[i]) dfs(i); 
+
+    ll res = 0;
+    memset(ans, oo, sizeof ans);
+
+    for(int j = 1; j <= m; ++j) if(e[j].w != -1) {
+        int r = lca(e[j].u, e[j].v);
+
+        for(int i = findset(e[j].u); i != findset(r); i = findset(i)) {
+            ans[fw[i]] = e[j].w, pa[i] = fa[i][0], i = fa[i][0];
+        }
+        for(int i = findset(e[j].v); i != findset(r); i = findset(i)) {
+            ans[fw[i]] = e[j].w, pa[i] = fa[i][0], i = fa[i][0];
+        }
     }
-    printf("%lld\n", ans);
+    for(int i = 1; i <= k; ++i) {
+        if(ans[i] == oo) {
+            res = -1; break;
+        } 
+        res += ans[i];
+    }
+    printf("%lld\n", res);
     return 0;
 }
