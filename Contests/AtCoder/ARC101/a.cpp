5bff6e94f2655809007f6083e25d7a21e11375ea
wearrys <1721604476@qq.com>
2018-08-26 11:25:21 +0800

    arc101

---
+++ Contests/AtCoder/ARC101/a.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using std::pair;
+using std::vector;
+using std::string;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define fst first
+#define snd second
+#define pb(a) push_back(a)
+#define mp(a, b) std::make_pair(a, b)
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+
+template <typename T> bool chkmax(T& a, T b) { return a < b ? a = b, 1 : 0; }
+template <typename T> bool chkmin(T& a, T b) { return a > b ? a = b, 1 : 0; }
+
+const int oo = 0x3f3f3f3f;
+
+string procStatus() {
+    std::ifstream t("/proc/self/status");
+    return string(std::istreambuf_iterator<char>(t), std::istreambuf_iterator<char>());
+}
+
+template <typename T> T read(T& x) {
+    int f = 1; x = 0;
+    char ch = getchar();
+    for(;!isdigit(ch); ch = getchar()) if(ch == '-') f = -1;
+    for(; isdigit(ch); ch = getchar()) x = x * 10 + ch - 48;
+    return x *= f;
+}
+
+const int N = 100000;
+
+int n, k;
+int a[N + 5];
+
+int main() {
+#ifdef Wearry
+    freopen("in", "r", stdin);
+    freopen("out", "w", stdout);
+#endif
+
+    read(n), read(k);
+    for(int i = 1; i <= n; ++i) read(a[i]);
+
+    if(a[1] >= 0) return printf("%d\n", a[k]), 0;
+    if(a[n] <= 0) return printf("%d\n", -a[n-k+1]), 0;
+
+    int ans = INT_MAX;
+    for(int i = 1; i + k - 1 <= n; ++i) if(a[i] <= 0 && a[i + k - 1] >= 0) {
+        chkmin(ans, -a[i] + a[i + k - 1] - a[i]);
+        chkmin(ans, a[i + k - 1] * 2 - a[i]);
+    }
+    printf("%d\n", ans);
+
+    return 0;
+}
