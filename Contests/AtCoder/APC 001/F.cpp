e193d0ed60c6eeb1364b8646e72676480e7622a1
wearrys <1721604476@qq.com>
2018-09-07 21:39:04 +0800

    apc 001

--- Contests/AtCoder/APC 001/F.cpp
+++ Contests/AtCoder/APC 001/F.cpp
@@ -31,11 +31,50 @@ template <typename T> T read(T& x) {
     return x *= f;
 }
 
+const int N = 100000;
+const int S = 1 << 16;
+
+int n;
+int val[N + 5];
+bool can[S + 5];
+int dp[S + 5], cnt[16];
+
 int main() {
 #ifdef Wearry
     freopen("in", "r", stdin);
     freopen("out", "w", stdout);
 #endif
+
+    scanf("%d", &n);
+    for(int i = 1; i < n; ++i) {
+        static int u, v, w;
+        scanf("%d%d%d", &u, &v, &w);
+        val[u] ^= w;
+        val[v] ^= w;
+    }
+
+    for(int i = 1; i <= n; ++i) {
+        ++ cnt[val[i]];
+    }
+
+    int ans = 0, mask = 0;
+    for(int i = 1; i < 16; ++i) {
+        ans += cnt[i] / 2;
+        if(cnt[i] &= 1) mask |= 1 << i;
+    }
+
+    for(int i = 0; i < S; ++i) {
+        int res = 0;
+        for(int j = 0; j < 16; ++j) if(i >> j & 1) res ^= j;
+        if(res == 0) can[i] = true;
+    }
+
+    for(int i = 0; i < S; ++i) {
+        for(int j = i; j; j = (j - 1) & i) 
+            if(can[j]) { chkmax(dp[i], dp[i ^ j] + 1); }
+    }
+
+    printf("%d\n", ans + __builtin_popcount(mask) - dp[mask]);
 
     return 0;
 }
