28f47ee28bd88258c7755057d516ae9ebc020de0
wearrys <1721604476@qq.com>
2018-11-28 21:59:46 +0800

    CT

--- Contests/CT2018/11-26/a.cpp
+++ Contests/CT2018/11-26/a.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
-#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 100;
+const int N = 300;
+const int oo = 0x3f3f3f3f;
 
-void chkmin(int&a, int b) {
-    (a > b) ? a = b : 0;
-}
+template <typename T> inline bool chkmin(T& a, T b) { return (a > b) ? a = b, 1 : 0; }
 
 int n;
 long long a[N + 5];
-int f[N + 5][N + 5][N + 5][N + 5];
+int c[N + 5][N + 5];
+int g[N +  5][N + 5], f[N + 5][N + 5];
+int gl[N + 5][N + 5], gr[N + 5][N + 5];
+int fl[N + 5][N + 5][N + 5], fr[N + 5][N + 5][N + 5];
+int pl[N + 5][N + 5][N + 5], pr[N + 5][N + 5][N + 5];
 
 int main() {
 #ifdef Wearry
@@ -20,97 +22,51 @@ int main() {
 
     scanf("%d", &n);
     for(int i = 1; i <= n; ++i) scanf("%lld", a + i);
-
-    memset(f, 0x3f, sizeof f);
-
     for(int i = 1; i <= n; ++i) {
         vector<long long> vec;
         for(int j = 1; j <= n; ++j) vec.push_back(abs(a[i] - a[j]));
-
         sort(vec.begin(), vec.end());
-        for(int j = 1; j <= i; ++j) {
-            for(int k = i; k <= n; ++k) {
-                f[i][i][j][k] = 
-                    upper_bound(vec.begin(), vec.end(), max(abs(a[i] - a[j]), abs(a[i] - a[k]))) - vec.begin();
+        for(int j = 1; j <= n; ++j) {
+            c[i][j] = upper_bound(vec.begin(), vec.end(), abs(a[i] - a[j])) - vec.begin();
+        }
+    }
+
+    for(int d = 2; d <= n; ++d) {
+        for(int l = 1; l + d - 1 <= n; ++l) {
+            int r = l + d - 1;
+            gl[l][r] = gr[l][r] = f[l][r] = oo;
+            for(int m = l; m < r; ++m) { chkmin(f[l][r], gl[l][m] + gr[m+1][r]); }
+            for(int m = l; m < r; ++m) {
+                fl[l][m][r] = (l == m) ? f[m][r] : oo;
+                for(int p = l; p < m; ++p) 
+                    chkmin(fl[l][m][r], fl[l][p][m] + f[m][r] + max(c[m][p], c[m][r]));
+                chkmin(gl[l][r], fl[l][m][r] + c[r][m]); 
+                pl[l][m][r] = fl[l][m][r] + c[m][r];
+                if(m > l) chkmin(pl[l][m][r], pl[l][m-1][r]);
+            }
+            for(int m = r; m > l; --m) {
+                fr[l][m][r] = (r == m) ? f[l][m] : oo;
+                for(int p = r; p > m; --p) 
+                    chkmin(fr[l][m][r], fr[m][p][r] + f[l][m] + max(c[m][p], c[m][l]));
+                chkmin(gr[l][r], fr[l][m][r] + c[l][m]);
             }
         }
     }
 
-    for(int l = 2; l <= n; ++l) {
-        for(int i = 1; i + l - 1 <= n; ++i) {
-            int j = i + l - 1;
-            for(int k = i; k < j; ++k) {
-                for(int c = 1; c <= i; ++c) {
-                    for(int d = j; d <= n; ++d) {
-                        chkmin(f[i][j][c][d], f[i][k][c][d] + f[k+1][j][i][j]);
-                        chkmin(f[i][j][c][d], f[i][k][i][d] + f[k+1][j][c][j]);
-                        chkmin(f[i][j][c][d], f[i][k][c][j] + f[k+1][j][i][d]);
-                        chkmin(f[i][j][c][d], f[i][k][i][j] + f[k+1][j][c][d]);
-                    }
+    for(int l = 1; l <= n; ++l) {
+        for(int r = l + 1; r <= n; ++r) {
+            g[l][r] = oo;
+            if(l == 1) g[l][r] = f[l][r] + c[l][r];
+            else {
+                for(int m = 1; m < l; ++m) {
+                    chkmin(g[l][r], g[m][l] + max(c[l][m], c[l][r]) + f[l][r]);
                 }
             }
         }
     }
-    printf("%d\n", f[1][n][1][n]);
 
-    return 0;
-}
-using namespace std;
-
-const int N = 100;
-
-void chkmin(int&a, int b) {
-    (a > b) ? a = b : 0;
-}
-
-int n;
-long long a[N + 5], r[N + 5];
-vector<long long> dis[N + 5];
-
-int fa[N + 5];
-int findset(int x) {
-    return (fa[x] == x) ? fa[x] : fa[x] = findset(fa[x]);
-}
-
-int ans = 0x3f3f3f3f;
-void dfs(int cur, int cnt = 0) {
-    if(cnt + (n - cur + 1) >= ans) return;
-    if(cur > n) {
-        for(int i = 1; i <= n; ++i) fa[i] = i;
-        for(int i = 1; i <= n; ++i) {
-            for(int j = i+1; j <= n; ++j) if(min(r[i], r[j]) >= abs(a[i] - a[j])) {
-                fa[findset(i)] = findset(j);
-            }
-        }
-        for(int i = 1; i <= n; ++i) if(findset(i) != findset(1)) return;
-        ans = cnt; 
-        return;
-    }
-
-    for(int i = 0; i < (int) dis[cur].size(); ++i) {
-        r[cur] = dis[cur][i];
-        int _cnt = cnt;
-        for(int j = 1; j <= n; ++j) _cnt += (r[cur] >= abs(a[cur] - a[j]));
-        dfs(cur + 1, _cnt);
-    }
-}
-
-int main() {
-#ifdef Wearry
-    freopen("in", "r", stdin);
-    freopen("out", "w", stdout);
-#endif
-
-    scanf("%d", &n);
-    for(int i = 1; i <= n; ++i) scanf("%lld", a + i);
-    for(int i = 1; i <= n; ++i) {
-        for(int j = 1; j <= n; ++j) dis[i].push_back(abs(a[i] - a[j]));
-        sort(dis[i].begin(), dis[i].end());
-        dis[i].erase(unique(dis[i].begin(), dis[i].end()), dis[i].end());
-        reverse(dis[i].begin(), dis[i].end());
-    }
-
-    dfs(1);
+    int ans = oo;
+    for(int i = 1; i < n; ++i) chkmin(ans, g[i][n] + c[n][i]);
     printf("%d\n", ans);
 
     return 0;
